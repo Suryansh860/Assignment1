947b0517572b663cbbd32816b0a2cf3958813051
Suryansh Dwivedi <suryanshd@infinite.com>
2024-02-28 13:06:00 +0530

    test code 28 feb

---
+++ ConsoleApp1/ConsoleApp1/class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(testcase("abcd"));
+            Console.WriteLine(testcase("a"));
+            Console.WriteLine(testcase("xy"));
+            Console.ReadLine();
+        }
+        public static string testcase(string str1)
+        {
+            return str1.Length > 1 ? str1.Substring(str1.Length - 1) + str1.Substring(1, str1.Length - 2) + str1.Substring(0, 1) : str1;
+        }
+    }
+
+}
+
