3fa948bd13125827ec14e2d8ca3fdda5e17aa760
Suryansh Dwivedi <161496431+Suryansh860@users.noreply.github.com>
2024-03-06 10:54:10 +0530

    Add files via upload

---
+++ Code_Test2/Code_Test2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Code_Test2
+{
+    abstract class Student
+    {
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public double Grade {  get; set; }
+
+        public abstract bool IsPassed(double grade);
+
+    }
+       class Undergraduate : Student
+            {
+             public Undergraduate(string name, int studentId, double grade)
+                {
+                 Name = name;
+                 StudentId = studentId;
+                 Grade = grade;
+                }
+            public override bool IsPassed(double grade)
+            {
+                return grade > 70.0;
+            }
+        }
+        
+        class Graduate : Student
+        {
+            public Graduate(string name, int studentId, double grade)
+              {
+               Name = name;
+               StudentId = studentId;
+               Grade = grade;
+              }
+        public override bool IsPassed(double grade)
+        {
+            return grade > 80.0;
+        }
+    }
+      class Program
+        { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name for UnderGraduate:");
+            String underGraduateName = Console.ReadLine();
+            Console.WriteLine("Enter the StudentId");
+            int underGraduateId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Grade:");
+            double grade = double.Parse(Console.ReadLine());
+
+            Undergraduate un = new Undergraduate(underGraduateName,underGraduateId, grade);
+
+            Console.WriteLine("Enter the name for Graduate:");
+            String graduateName = Console.ReadLine();
+            Console.WriteLine("Enter the StudentId");
+            int graduateId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Grade:");
+            double graduategrade = double.Parse(Console.ReadLine());
+
+            Graduate gr = new Graduate(graduateName, graduateId, graduategrade);
+
+
+            Console.WriteLine($"Undergraduate passed: {un.IsPassed(un.Grade)}");
+            Console.WriteLine($"Graduate passed: {gr.IsPassed(gr.Grade)}");
+            Console.ReadLine();
+    }
+    }
+}
