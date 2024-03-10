adf475b16e33f74890d53a686cdfe318fe6ccd4b
Suryansh Dwivedi <161496431+Suryansh860@users.noreply.github.com>
2024-03-11 00:52:45 +0530

    Add files via upload

---
+++ Assignment_4/LambdaQuery_1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    internal class LambdaQuery_1
+    {
+        static void Main(string[] args)
+        {
+            //taking user input which is seperated by commas...
+            Console.WriteLine("Enter numbers by seperating them commas:");
+            string s = Console.ReadLine();
+
+            //Split the input string by commas and converting each part into an Integer....
+            int[] numbers = s.Split(',').Select(int.Parse).ToArray();
+
+            //Selecting the square of each number and filtering those numbers which are greater than 20
+            var result = numbers.Select(n => n * n).Where(square => square > 20);
+
+            //Displaying the Result or Numbers (which are greater than 20)
+
+            Console.WriteLine("Square greater than 20: ");
+            foreach (var square in result)
+            {
+                Console.WriteLine(square);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment_4/LambdaQuery_2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    internal class LambdaQuery_2
+    {
+        static void Main(string[] args)
+        {
+            //Taking user input words which is seperated by commas....
+            Console.WriteLine("Enter the words seperated by commas:");
+            string s = Console.ReadLine();
+
+            //Spliting the input string into an array of words....
+            string[] words = s.Split(',');
+
+            //Query to filtering the words which is starting with 'a' and ending with 'm'.....
+            var Result = words.Where(word => word.Trim().StartsWith("a", StringComparison.OrdinalIgnoreCase) && word.Trim().EndsWith("m", StringComparison.OrdinalIgnoreCase));
+
+            //Printing the Output...
+
+            Console.WriteLine("Words starting with 'a' and ending with 'm':");
+            foreach (var word in Result)
+            {
+                Console.WriteLine(word.Trim());
+            }
+            Console.ReadLine();
+        }
+    }
+}
